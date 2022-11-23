c9e0f8765a66ad7402f4e5a101365cc1a6dea0bc
Max <mkorobov2002@mail.ru>
2022-11-24 00:18:13 +0300

    BaseDrawer JSX Added

--- frontend/src/components/elements/drawer/BaseDrawer/index.js
+++ frontend/src/components/elements/drawer/BaseDrawer/index.js
@@ -1,5 +1,10 @@
+import { Drawer } from '@mui/material';
 import React from 'react';
 
-export default function BaseDrawer() {
-  return <div>BaseDrawer</div>;
+export default function BaseDrawer({ children, anchor, open, onClose }) {
+  return (
+    <Drawer anchor={anchor} open={open} onClose={onClose}>
+      {children}
+    </Drawer>
+  );
 }
