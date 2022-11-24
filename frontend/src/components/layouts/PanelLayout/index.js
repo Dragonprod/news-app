7a659658c321e4e617b98726baa55a4e8700b1d2
Max <mkorobov2002@mail.ru>
2022-11-24 10:51:45 +0300

    NewsDrawer Imported

--- frontend/src/components/layouts/PanelLayout/index.js
+++ frontend/src/components/layouts/PanelLayout/index.js
@@ -1,11 +1,21 @@
 import { Stack } from '@mui/material';
-import React from 'react';
+import React, { useContext } from 'react';
+import { MainContext } from '../../../context/MainContextProvider';
+import NewsDrawer from '../../elements/drawer/NewsDrawer';
 import NewsTable from '../../elements/tables/NewsTable';
 
 export default function PanelLayout() {
+  const { isNewsDrawerOpen, setIsNewsDrawerOpen } = useContext(MainContext);
+
   return (
-    <Stack sx={{ gap: '32px', m: '64px auto', maxWidth: 'var(--max-width)' }}>
-      <NewsTable />
-    </Stack>
+    <>
+      <Stack sx={{ gap: '32px', m: '64px auto', maxWidth: 'var(--max-width)' }}>
+        <NewsTable />
+      </Stack>
+      <NewsDrawer
+        open={isNewsDrawerOpen}
+        onClose={() => setIsNewsDrawerOpen(false)}
+      />
+    </>
   );
 }
